45b6eb812c525b8726cf08a04211ba6c66aaa8e1
nguyendinhduc <uet.nguyenduc@gmail.com>
2020-08-05 12:06:29 +0700

    no comment

--- src/com/vmodev/demostring/Main.java
+++ src/com/vmodev/demostring/Main.java
@@ -2,6 +2,8 @@ package com.vmodev.demostring;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(upperCaseFirstChar);
+        System.out.println(new MyString().upperCaseFirstChar("Hello Test ahihi"));
+        System.out.println("Test................");
+//        System.out.println(upperCaseFirstChar);
     }
 }
